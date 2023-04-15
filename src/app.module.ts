7a155273b91a6240ea668d173148de906399738a
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2023-04-15 02:09:57 +0100

    add APP_PIPE and ValidationPipe in providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { BeneficiariesModule } from './beneficiaries/beneficiaries.module';
 import { PrismaService } from './database/prisma.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [BeneficiariesModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    }
+  ]
 })
 export class AppModule {}
