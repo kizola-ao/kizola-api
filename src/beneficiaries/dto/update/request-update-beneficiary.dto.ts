546a0c14e608345e4179d7f6602a1becaedde61a
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2023-04-15 15:38:41 +0100

    add class-validator decorators on properties

--- src/beneficiaries/dto/update/request-update-beneficiary.dto.ts
+++ src/beneficiaries/dto/update/request-update-beneficiary.dto.ts
@@ -1,15 +1,69 @@
+import { ArrayNotEmpty, ArrayUnique, IsEmail, IsInt, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator"
+
 export class RequestUpdateBeneficiaryDto {
+    
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     name: string | null
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     logo: string | null
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     description: string | null
-    socialCausesId: [] | null
-    phoneNumbers: [] | null
-    emails: [] | null
-    provinceId: number | null
-    street: string | null
-    countyId: number | null
-    neighborhoodId: number | null
-    neighborhoodName: string | null
-    referencePoint: string | null
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     biography: string | null
+
+    @IsOptional()
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsUUID('4', { each: true })
+    socialCausesId: [] | null
+
+    @IsOptional()
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsString({ each: true })
+    phoneNumbers: [] | null
+
+    @IsOptional()
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsEmail({}, { each: true })
+    emails: [] | null
+
+    @IsOptional()
+    @IsInt()
+    provinceId: number | null
+
+    @IsOptional()
+    @IsInt()
+    countyId: number | null
+
+    @IsOptional()
+    @IsInt()
+    neighborhoodId: number | null
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    neighborhoodName: string | null
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    street: string | null
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    referencePoint: string | null
 };
