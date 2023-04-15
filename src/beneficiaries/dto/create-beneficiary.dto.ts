7602c52324cb1899f77c772b2a9ce8816b07f13f
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2023-04-15 02:12:40 +0100

    add class-validator decorators on properties

--- src/beneficiaries/dto/create-beneficiary.dto.ts
+++ src/beneficiaries/dto/create-beneficiary.dto.ts
@@ -1,15 +1,59 @@
+import { ArrayNotEmpty, ArrayUnique, IsEmail, IsInt, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator"
+
 export class CreateBeneficiaryDto {
+    @IsNotEmpty()
+    @IsString()
     name: string
+
+    @IsNotEmpty()
+    @IsString()
     logo: string
+
+    @IsNotEmpty()
+    @IsString()
     description: string
-    socialCausesId: []
-    phoneNumbers: []
-    emails: []
-    provinceId: number
-    street: string
-    countyId: number
-    neighborhoodId: number
-    neighborhoodName: string
-    referencePoint: string
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     biography: string | null
+    
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsUUID('4', { each: true })
+    socialCausesId: []
+
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsString({ each: true })
+    phoneNumbers: []
+
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsEmail({}, { each: true })
+    emails: []
+
+    @IsInt()
+    provinceId: number
+
+    @IsInt()
+    countyId: number
+
+    @IsOptional()
+    @IsInt()
+    neighborhoodId: number | null
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    neighborhoodName: string | null
+
+    @IsNotEmpty()
+    @IsString()
+    street: string
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    referencePoint: string | null
 };
